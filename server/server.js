45d96aa343d6c7898a3d80fe1f885e34980b1f9b
Rahul-Patanwadia <patanwadiarahulr@gmail.com>
2023-08-01 15:41:51 +0530

    MongoDB setup

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
 
+require('dotenv').config();
 const xss = require('xss-clean')
 const mongoose = require('mongoose')
 const mongoSanitize = require('express-mongo-sanitize')
 const routes = require('./routes')
+
+const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}?retryWrites=true&w=majority`
+
+mongoose.connect(mongoUri,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 //body parse
 app.use(express.json())
